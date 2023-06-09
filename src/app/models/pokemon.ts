18f3475deabba07455b0789f7c9873faeb5c73b6
poggersalex <alexander@steeleconsult.com>
2023-06-09 13:38:30 -0600

    UPDATE UI

--- src/app/models/pokemon.ts
+++ src/app/models/pokemon.ts
@@ -6,7 +6,10 @@ export class Pokemon{
     }[];
     base_experience? : number;
     forms? : {name? : 'string', url? : string}[];
-    game_index? : {game_index? : number, version? : {name? : string, url? : string}}[];
+    game_indices? : {
+        game_index? : number,
+        version? : {name? : string, url? : string}
+    }[];
     height? : number;
     held_items? : {item? : {name? : string, url? : string}}[];
     id? : number;
